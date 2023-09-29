1dd22f704555c91b4f16726b7e7d05f9b6a05314
St0pthink <foljustfol@gmail.com>
2023-09-29 15:51:11 +0500

    up 15

--- 1 Semestr (Py)/2 Unit/5.1.2.py
+++
@@ -1,7 +0,0 @@
-from math import *
-
-
-x = float(input("Введите значение x:"))
-t = float(input("Введите значение t, не равное 2*Pi*n, n - любое целое:"))
-f = (1 + x**2 - 2*x*t)/ sin(t) + 1 + sqrt(1 + sin(x)**2 + cos(x*t)**2)
-print("Значение функции f(x;t) в точке x= {:.2f} и в точке t = {:.2f} равно {:.2f}".format(x,t,f))
--- 5.1.1.py
+++
@@ -1,6 +0,0 @@
-from math import *
-
-
-x = float(input("Введите значение x, не равное 0:"))
-y = abs(sin((1+x**(1/3))/x))
-print("Значение функции y в точке x={:.2f} равно {:.2f}".format(x,y))
